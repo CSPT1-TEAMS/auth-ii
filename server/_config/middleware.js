0059ce6caf55119a8e45abe4579abc5a8dcef5bd
Glenn-David Daniel <d2rd@outlook.com>
2018-06-20 23:25:29 -0700

    Added missing modules calls to middleware.js

--- server/_config/middleware.js
+++ server/_config/middleware.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
 const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 
-module.exports = function(server) {
-  server.use(helmet());
-  server.use(morgan('dev'));
-  server.use(express.json());
-  server.use(cors());
-  server.use(jwt);
-};
+module.exports = function (server) {
+  server.use(helmet())
+  server.use(morgan('dev'))
+  server.use(express.json())
+  server.use(cors())
+  server.use(jwt)
+  server.use(mongoose)
+  server.use(MongoStore)
+}
--- server/server.js
+++ server/server.js
@@ -6,23 +6,23 @@ const MongoStore = require('connect-mongo')(session)
 const routes = require('./_config/routes')
 const restrictedRoutes = require('./auth/authRoutes')
 
-const db = require('./_config/db');
-const setupMiddleware = require('./_config/middleware');
-const setupRoutes = require('./_config/routes');
+const db = require('./_config/db')
+const setupMiddleware = require('./_config/middleware')
+const setupRoutes = require('./_config/routes')
 
-setupMiddleware(server);
-setupRoutes(server);
+setupMiddleware(server)
+setupRoutes(server)
 
 db.connectTo('authii')
   .then(() => {
-    console.log('\n... API Connected to authii Database ...\n');
+    console.log('\n... API Connected to authii Database ...\n')
     server.listen(5500, () =>
       console.log('\n=== API running on port 5500 ===\n')
-    );
+    )
   })
   .catch(err => {
-    console.log('\n*** ERROR Connecting to MongoDB, is it running? ***\n', err);
-  });
+    console.log('\n*** ERROR Connecting to MongoDB, is it running? ***\n', err)
+  })
 
 const { makeToken, verifyToken } = require('./authFunctions')
 
