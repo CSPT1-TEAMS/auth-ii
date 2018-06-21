581f564c4dbc7d3dd957734aefe42b9c53e89092
Charlie <ccariste@gmail.com>
2018-06-20 20:54:12 -0700

    CRC: Register working, needed to catch an error

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -3,6 +3,7 @@ const User = require('../users/User');
 const { makeToken } = require('./jwt');
 
 router.post('/register', function(req, res) {
+  console.log(req.body)
   User.create(req.body)
     .then(({ username, race }) => {
       // we destructure the username and race to avoid returning the hashed password
@@ -10,7 +11,7 @@ router.post('/register', function(req, res) {
       // then we assemble a new object and return it
       res.status(201).json({ username, race });
     })
-    .catch(err => res.status(500).json(err));
+    .catch(err => res.status(500).json({msg: 'Internal Server Error!?', err}));
 });
 
 router.post('/login', function(req, res) {
