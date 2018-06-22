c7ac24f5d3cd460acfada5ecf3c25fd6974a441e
darthMinimus <irishseanvaldivia@gmail.com>
2018-06-21 21:12:32 -0500

    Trying to get input to work.

--- client/src/register.js
+++ client/src/register.js
@@ -1,29 +1,42 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 class Register extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            username:'',
-            password:'',
-            race:''
+            username: '',
+            password: '',
+            race: ''
         }
     }
 
+    submitInput = e => {
+        e.preventDefault();
+        axios.post('localhost:5500/api/auth/register', {username: this.state.username, password: this.state.password, race: this.state.race})
+            .then(function(response) {
+                console.log(response)
+            })
+            .catch()
+        
+        this.setState({
+         
+        })
+      }
+
     handleChange = e => {
         e.preventDefault()
-        this.setState({[e.target.value]:e.target.value})
+        this.setState({ [e.target.value]: e.target.value })
     }
 
     render() {
-        return(
+        return (
             <div>
                 <form>
 
-            <input type = "text" value = {this.state.username} onChange = {this.handleChange}  placeholder = "username"/> 
-            <input onChange = {this.handleChange} value = {this.state.password} placeholder = "password"/> 
-            <input onChange = {this.handleChange} value = {this.state.race} placeholder = "race"/>
-               </form>
+                    <input type="text" value={this.state.username} onChange={this.handleChange} placeholder="username" />
+                    <input onChange={this.handleChange} value={this.state.password} placeholder="password" />
+                    <input onChange={this.handleChange} value={this.state.race} placeholder="race" />
+                </form>
             </div>
         )
     }
