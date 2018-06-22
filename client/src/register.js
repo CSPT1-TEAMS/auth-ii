d0252925545e65b3c94a3bac8b100099b8064398
JohnCoronel <john.m.coronel@gmail.com>
2018-06-21 18:50:40 -0700

    register form

--- client/src/register.js
+++ client/src/register.js
@@ -4,19 +4,25 @@ class Register extends Component {
     constructor(){
         super();
         this.state = {
-
+            username:'',
+            password:'',
+            race:''
         }
     }
 
-
+    handleChange = e => {
+        e.preventDefault()
+        this.setState({[e.target.value]:e.target.value})
+    }
 
     render() {
         return(
             <div>
                 <form>
-            <input placeholder = "username"/> 
-            <input placeholder = "password"/> 
-            <input placeholder = "race"/>
+
+            <input type = "text" value = {this.state.username} onChange = {this.handleChange}  placeholder = "username"/> 
+            <input onChange = {this.handleChange} value = {this.state.password} placeholder = "password"/> 
+            <input onChange = {this.handleChange} value = {this.state.race} placeholder = "race"/>
                </form>
             </div>
         )
